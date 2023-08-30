38f9d4539d732c2559ee4fad8d256f9ed6cf9f8e
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-31 00:42:01 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RXJS-bufferToggle-2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-bfnu7r)
---
+++ index.ts
@@ -0,0 +1,23 @@
+import { interval } from 'rxjs';
+import { bufferToggle } from 'rxjs/operators';
+
+// https://www.learnrxjs.io/learn-rxjs/operators/transformation/buffertoggle
+// Example 1: Toggle buffer on and off at interval
+
+//emit value every second
+const sourceInterval = interval(1000);
+//start first buffer after 5s, and every 5s after
+const startInterval = interval(5000);
+//emit value after 3s, closing corresponding buffer
+const closingInterval = (val) => {
+  console.log(`Value ${val} emitted, starting buffer! Closing in 3s!`);
+  return interval(3000);
+};
+//every 5s a new buffer will start, collecting emitted values for 3s then emitting buffered values
+const bufferToggleInterval = sourceInterval.pipe(
+  bufferToggle(startInterval, closingInterval)
+);
+// output: [4,5,6]...[9,10,11]
+const subscribe = bufferToggleInterval.subscribe((val) =>
+  console.log('Emitted Buffer:', val)
+);
